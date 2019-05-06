9a3af2eda0ba8b1282fd5b8fa712d4d3ab7d27bc
anderstofte <ande587e@stud.kea.dk>
2019-05-06 13:14:19 +0200

    implemented router

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="login-outer">
+        <div>
+            jes
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+.login-outer {
+    background-color: yellow;
+    height: 100%;
+}
+</style>
